ccd2709b2571f27eb209c0898abff537ccc02638
David Oniani <onianidavid@gmail.com>
2020-05-11 13:38:03 -0500

    Update repository

---
+++ annotations/average.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+"""
+Filename: average.py
+Author:   David Oniani
+E-mail:   oniani.david@mayo.edu
+
+Description:
+    Report average metrics.
+"""
+
+import numpy as np
+import pandas as pd
+
+
+def avg_approach(df: pd.DataFrame, name: str) -> float:
+    return (df.loc[df["Approach"] == name]["Answer Relevance (1-5)"]).mean()
+
+
+if __name__ == "__main__":
+    annotator_1 = pd.read_csv("annotator_1.csv")
+    annotator_2 = pd.read_csv("annotator_2.csv")
+
+    print("Annotator 1")
+    print("===========")
+
+    print(
+        "TfidfVectorizer + Cosine:",
+        avg_approach(
+            annotator_1,
+            "TfidfVectorizer (Scikit-learn) + Cosine Similarity "
+            "(Scikit-learn)",
+        ),
+    )
+
+    print(
+        "BERT:",
+        avg_approach(
+            annotator_1,
+            "BERT-Large, Uncased: 24-layer, 1024-hidden, 16-heads, 340M "
+            "parameters + Cosine Similarity (Scikit-learn)",
+        ),
+    )
+
+    print(
+        "BioBERT:",
+        avg_approach(
+            annotator_1,
+            "BioBERT-Large v1.1 (+ PubMed 1M) - based on BERT-large-Cased "
+            "(custom 30k vocabulary) + Cosine Similarity (Scikit-learn)",
+        ),
+    )
+
+    print(
+        "USE:",
+        avg_approach(
+            annotator_1,
+            "Universal Sentence Encoder Version 3 Large + Inner Product "
+            "(numpy)",
+        ),
+    )
+
+    ###########################################################################
+
+    print("\n")
+
+    ###########################################################################
+
+    print("Annotator 2")
+    print("===========")
+
+    print(
+        "TfidfVectorizer + Cosine:",
+        avg_approach(
+            annotator_2,
+            "TfidfVectorizer (Scikit-learn) + Cosine Similarity "
+            "(Scikit-learn)",
+        ),
+    )
+
+    print(
+        "BERT:",
+        avg_approach(
+            annotator_2,
+            "BERT-Large, Uncased: 24-layer, 1024-hidden, 16-heads, 340M "
+            "parameters + Cosine Similarity (Scikit-learn)",
+        ),
+    )
+
+    print(
+        "BioBERT:",
+        avg_approach(
+            annotator_2,
+            "BioBERT-Large v1.1 (+ PubMed 1M) - based on BERT-large-Cased "
+            "(custom 30k vocabulary) + Cosine Similarity (Scikit-learn)",
+        ),
+    )
+
+    print(
+        "USE:",
+        avg_approach(
+            annotator_2,
+            "Universal Sentence Encoder Version 3 Large + Inner Product "
+            "(numpy)",
+        ),
+    )
--- annotations/iaa.py
+++ annotations/iaa.py
@@ -11,13 +11,14 @@ Description:
 """
 
 import numpy as np
+import pandas as pd
 
 from sklearn import metrics
 
 
 if __name__ == "__main__":
-    annotator_1 = [3, 3, 1, 1, 5, 4, 5, 3, 2, 2, 1, 5, 4, 4, 4, 3, 4, 3, 4, 4]
-    annotator_2 = [1, 5, 4, 4, 5, 2, 4, 3, 5, 1, 2, 5, 4, 5, 5, 3, 5, 4, 5, 4]
+    annotator_1 = pd.read_csv("annotator_1.csv")["Answer Relevance (1-5)"]
+    annotator_2 = pd.read_csv("annotator_2.csv")["Answer Relevance (1-5)"]
 
     print(
         "Cohen's Kappa:", metrics.cohen_kappa_score(annotator_1, annotator_2)
